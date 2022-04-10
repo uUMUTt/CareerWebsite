3f3f1757b6bea36fd53ad28fd25ced532cc98324
RamazanKpk <kopukramazan2@gmail.com>
2022-04-10 17:03:14 +0300

    UserDAO oluşturuldu

--- src/src/java/dao/UserDAO.java
+++ src/src/java/dao/UserDAO.java
@@ -32,26 +32,37 @@ public class UserDAO extends SuperDAO<User>{
 
     @Override
     public User createEntity(ResultSet rs) {
+        User u = null;
+        try {
+            u = new User(rs.getInt("id"), rs.getString("mail"), rs.getNString("password"));
+        } catch (Exception e) {
+        }
+        return u;
     }
 
     @Override
     public String createQueryReadByID(int id) {
+        return "select * from User where id='" + id + "'";
     }
 
     @Override
     public String createQueryDelete(User t) {
+        return "delete from User where id='" + t.getId() + "'";
     }
 
     @Override
     public String createQueryUpdate(User t) {
+        return "update User set mail='" + t.getMail() + "' , password='" + t.getPassword() + "'";
     }
 
     @Override
     public String createQueryReadAll() {
+        return "select * from Company";
     }
 
     @Override
     public String createQueryInsert(User t) {
+        return "insert into CompanyType (title) values ('" + t.getMail()+ "' , '" + t.getPassword()+ "')";
     }
     
 }
