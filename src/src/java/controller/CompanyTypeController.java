fc86332d636b575f829b49f53923883533415920
uUMUTt <zeyen4421@gmail.com>
2022-04-11 00:14:32 +0300

    CompanyTypeController created

---
+++ src/src/java/controller/CompanyTypeController.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author kalix
+ */
+@Named(value = "companyTypeController")
+@SessionScoped
+public class CompanyTypeController implements Serializable {
+
+    /**
+     * Creates a new instance of CompanyTypeController
+     */
+    public CompanyTypeController() {
+    }
+    
+}
