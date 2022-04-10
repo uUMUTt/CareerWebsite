fb1ae19eca8a4cac8a9ed1b9dd98a434901f18ee
alitayyipgumus44 <atgumus44@gmail.com>
2022-04-10 16:49:32 +0300

    CompanyTypeDAO sınıfının içerisindeki eksikler giderildi.

--- src/src/java/dao/CompanyTypeDAO.java
+++ src/src/java/dao/CompanyTypeDAO.java
@@ -4,6 +4,7 @@
  */
 package dao;
 
+import entity.Company;
 import entity.CompanyType;
 import java.sql.ResultSet;
 
@@ -11,48 +12,55 @@ import java.sql.ResultSet;
  *
  * @author kalix
  */
-public class CompanyTypeDAO extends SuperDAO<CompanyType>{
-    
+public class CompanyTypeDAO extends SuperDAO<CompanyType> {
+
     public CompanyType findByID(int id) {
         CompanyType ct = null;
         try {
-            String query  = "select * from CompanyType where id='"+id+"'";
+            String query = "select * from CompanyType where id='" + id + "'";
             ResultSet rs = this.st.executeQuery(query);
-            while(rs.next()) {
+            while (rs.next()) {
                 ct = new CompanyType(rs.getInt("id"), rs.getString("title"));
             }
         } catch (Exception e) {
         }
         return ct;
     }
-    
-    
-    
-    
-    //***********************************************************
 
+    //***********************************************************
     @Override
     public CompanyType createEntity(ResultSet rs) {
+        CompanyType ct = null;
+        try {
+            ct = new CompanyType(rs.getInt("id"), rs.getString("title"));
+        } catch (Exception e) {
+        }
+        return ct;
     }
 
     @Override
     public String createQueryReadByID(int id) {
+        return "select * from CompanyType where id='" + id + "'";
     }
 
     @Override
     public String createQueryDelete(CompanyType t) {
+        return "delete from CompanyType where id='" + t.getId() + "'";
     }
 
     @Override
     public String createQueryUpdate(CompanyType t) {
+        return "update CompanyType set title='" + t.getTitle() +"'";
     }
 
     @Override
     public String createQueryReadAll() {
+        return "select * from CompanyType";
     }
 
     @Override
     public String createQueryInsert(CompanyType t) {
+        return "insert into CompanyType (title) values ('" + t.getTitle() + "')";
     }
-    
+
 }
