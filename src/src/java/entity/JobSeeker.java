972dec13721174eaa84198a24caaba614d0034fe
RamazanKpk <kopukramazan2@gmail.com>
2022-04-10 14:19:38 +0300

    JobSeeker eklendi

---
+++ src/src/java/entity/JobSeeker.java
@@ -0,0 +1,154 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import com.oracle.wls.shaded.org.apache.bcel.generic.DADD;
+
+/**
+ *
+ * @author kopuk
+ */
+public class JobSeeker {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int gender;
+    private String birthDate;
+    private String address;
+    private String city;
+    private String mobile;
+    private int degreeId;
+    private String passYear;
+    private String detail;
+    private int experience;
+    private int userId;
+    
+    public JobSeeker(){
+        
+    }
+
+    public JobSeeker(int id, String firstName, String lastName, int gender, String birthDate, String address, String city, String mobile, int degreeId, String passYear, String detail, int experience, int userId) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.address = address;
+        this.city = city;
+        this.mobile = mobile;
+        this.degreeId = degreeId;
+        this.passYear = passYear;
+        this.detail = detail;
+        this.experience = experience;
+        this.userId = userId;
+    }
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public int getDegreeId() {
+        return degreeId;
+    }
+
+    public void setDegreeId(int degreeId) {
+        this.degreeId = degreeId;
+    }
+
+    public String getPassYear() {
+        return passYear;
+    }
+
+    public void setPassYear(String passYear) {
+        this.passYear = passYear;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    
+    
+}
