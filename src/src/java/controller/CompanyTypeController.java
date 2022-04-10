04c0e9d5ae08dababc6fbcf9d5a10232ae2d904e
uUMUTt <zeyen4421@gmail.com>
2022-04-11 00:52:43 +0300

    company_type.xhtml created

--- src/src/java/controller/CompanyTypeController.java
+++ src/src/java/controller/CompanyTypeController.java
@@ -1,25 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
- */
+
 package controller;
 
-import javax.inject.Named;
-import javax.enterprise.context.SessionScoped;
+import dao.CompanyTypeDAO;
+import entity.CompanyType;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  *
- * @author kalix
+ * @author Umut Karadas
  */
 @Named(value = "companyTypeController")
 @SessionScoped
 public class CompanyTypeController implements Serializable {
 
-    /**
-     * Creates a new instance of CompanyTypeController
-     */
+    private CompanyTypeDAO companyTypeDAO;
+    private CompanyType companyType;
+    private List<CompanyType> companyTypes;
+    
     public CompanyTypeController() {
     }
+    
+    public String create(){
+        companyTypeDAO.insert(companyType);
+        companyType = new CompanyType();
+        return "company_type";
+    }
+    
+    public String delete(CompanyType p) {
+        companyTypeDAO.delete(p);
+        return "company_type";
+    }
+    
+    public String update() {
+        companyTypeDAO.update(companyType);
+        companyType = new CompanyType();
+        return "company_type";
+    }
+
+    public CompanyTypeDAO getCompanyTypeDAO() {
+        if(this.companyTypeDAO == null) {
+            this.companyTypeDAO = new CompanyTypeDAO();
+        }
+        return companyTypeDAO;
+    }
+
+    public void setCompanyTypeDAO(CompanyTypeDAO companyTypeDAO) {
+        this.companyTypeDAO = companyTypeDAO;
+    }
+
+    public CompanyType getCompanyType() {
+        if(this.companyType == null) {
+            this.companyType = new CompanyType();
+        }
+        return companyType;
+    }
+
+    public void setCompanyType(CompanyType companyType) {
+        this.companyType = companyType;
+    }
+
+    public List<CompanyType> getCompanyTypes() {
+        this.companyTypes = this.getCompanyTypeDAO().readAll();
+        return companyTypes;
+    }
+
+    public void setCompanyTypes(List<CompanyType> companyTypes) {
+        this.companyTypes = companyTypes;
+    }
+    
     
 }
