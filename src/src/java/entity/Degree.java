aded9405a84787ba048b976853dbcd7d6d2e1da1
RamazanKpk <kopukramazan2@gmail.com>
2022-04-10 13:51:30 +0300

    Degree class eklendi

--- src/src/java/entity/Degree.java
+++ src/src/java/entity/Degree.java
@@ -9,5 +9,35 @@ package entity;
  * @author kopuk
  */
 public class Degree {
+    private int id;
+    private String title;
+    
+    public Degree(){
+        
+    }
+
+    public Degree(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
     
 }
+
