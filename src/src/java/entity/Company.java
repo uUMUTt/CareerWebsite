a936a065ac56d7840617152a479e1d5d66e43b49
alitayyipgumus44 <atgumus44@gmail.com>
2022-04-10 14:20:51 +0300

    Company varlık sınıfı eklendi.

---
+++ src/src/java/entity/Company.java
@@ -0,0 +1,121 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author ATG
+ */
+public class Company {
+    private int id;
+    private String companyName;
+    private String address; 
+    private String city;
+    private String contactPersonName;
+    private String contactPersonLastname;
+    private String mobile;
+    private int companyTypeID;
+    private String detail;
+    private int userID;
+
+    public Company() {
+    }
+
+    public Company(int id, String companyName, String address, String city, String contactPersonName, String contactPersonLastname, String mobile, int companyTypeID, String detail, int userID) {
+        this.id = id;
+        this.companyName = companyName;
+        this.address = address;
+        this.city = city;
+        this.contactPersonName = contactPersonName;
+        this.contactPersonLastname = contactPersonLastname;
+        this.mobile = mobile;
+        this.companyTypeID = companyTypeID;
+        this.detail = detail;
+        this.userID = userID;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getContactPersonName() {
+        return contactPersonName;
+    }
+
+    public void setContactPersonName(String contactPersonName) {
+        this.contactPersonName = contactPersonName;
+    }
+
+    public String getContactPersonLastname() {
+        return contactPersonLastname;
+    }
+
+    public void setContactPersonLastname(String contactPersonLastname) {
+        this.contactPersonLastname = contactPersonLastname;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public int getCompanyTypeID() {
+        return companyTypeID;
+    }
+
+    public void setCompanyTypeID(int companyTypeID) {
+        this.companyTypeID = companyTypeID;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+    
+    
+    
+}
