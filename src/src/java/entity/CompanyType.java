05c132d88c5e21727907d48cc54a684e4f5ce127
uUMUTt <zeyen4421@gmail.com>
2022-04-10 14:00:11 +0300

    CompanyType class added

---
+++ src/src/java/entity/CompanyType.java
@@ -0,0 +1,37 @@
+
+package entity;
+
+/**
+ *
+ * @author Umut Karadas
+ */
+public class CompanyType {
+    private int id;
+    private String title;
+    
+    public CompanyType(){
+        
+    }
+
+    public CompanyType(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
