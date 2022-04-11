8ce7584dff5f163ebdb5717dbec59e1c220701d0
uUMUTt <zeyen4421@gmail.com>
2022-04-11 14:20:20 +0300

    README.md upgrated - data.sql created

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CareerWebsite
 A website for job sharing and job searching.
+
+### Database ER Diagram
+
+![image](/docs/CareerWebsiteER.png)
+
+### Contributors
+- Umut Karadaş [uUMUTt]
+- Ramazan Köpük [RamazanKpk]
+- Ali Gümüş [alitayyipgumus44]
+- Murat Dayan [murat-dayan]
---
+++ scripts/data.sql
@@ -0,0 +1,25 @@
+INSERT INTO Skill (title) values ('Java');
+INSERT INTO Skill (title) values ('C++');
+INSERT INTO Skill (title) values ('Python');
+INSERT INTO Skill (title) values ('AI');
+INSERT INTO Skill (title) values ('Deep Learning');
+INSERT INTO Skill (title) values ('Syber Security');
+
+INSERT INTO Degree (title) values ('Graduate Degree');
+INSERT INTO Degree (title) values ('Bachelors Degree');
+INSERT INTO Degree (title) values ('Master Degree');
+INSERT INTO Degree (title) values ('PhD');
+
+INSERT INTO CompanyType (title) values ('Education');
+INSERT INTO CompanyType (title) values ('Engineering');
+INSERT INTO CompanyType (title) values ('IT');
+INSERT INTO CompanyType (title) values ('HR Department');
+
+INSERT INTO User (mail,password) values ('umut@gmail.com','12ed21dq');
+INSERT INTO User (mail,password) values ('ramazan@gmail.com','32rfdw');
+INSERT INTO User (mail,password) values ('ali@gmail.com','wqr2r44');
+INSERT INTO User (mail,password) values ('murat@gmail.com','23r2tw');
+INSERT INTO User (mail,password) values ('melis@gmail.com','d3232f');
+INSERT INTO User (mail,password) values ('ayse@gmail.com','d3232f');
+
+
